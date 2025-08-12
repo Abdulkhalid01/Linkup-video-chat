e043e51772a978651104489e53d9e20eacdf68ba
Alphaxcode1 <mohdkhalid3579@gmail.com>
2025-08-12 13:19:40 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ?  "http://localhost:8800/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ?  "http://localhost:8800/api" : "https://linkup-video-chat.onrender.com/api";
 
 export const axiosinstance = axios.create({
     baseURL: BASE_URL,
